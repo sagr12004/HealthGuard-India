ae1a437681136641e751c9ccbe369c0c50244184
sagr12004 <sagarssavadatti2004@gmail.com>
2026-03-09 17:02:57 +0530

    Fix: change risk route from /assess to /

--- backend/routes/risk.js
+++ backend/routes/risk.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const { assessRisk } = require("../controllers/riskController");
-router.post("/assess", assessRisk);
+router.post("/", assessRisk);
 module.exports = router;
